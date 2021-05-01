33fe598ddb7069cf943331d863b0e9828b393513
ma <mariusaltmann@outlook.de>
2021-05-02 01:38:10 +0200

    function now returns input twice - once without any changes - once with a added routingslip -> wrie-tap

--- mico-wire-tap-node/handler.js
+++ mico-wire-tap-node/handler.js
@@ -1,26 +1,12 @@
 "use strict";
 
 module.exports = async (functionInput, callback) => {
-    // var kafka = require('kafka-node'),
-    //     Producer = kafka.Producer,
-    //     client = new kafka.KafkaClient({ kafkaHost: '172.17.0.12:9092' }),
-    //     producer = new Producer(client);
+    const cloudEvent = JSON.parse(functionInput)
 
-    // var cloudEvent = JSON.parse(JSON.stringify(functionInput));
     let result = [];
-    // contextCopy["routingslip"] = [['secondary-channel']];
-    // let routingslip = [['secondary-channel']];
-    // let functionInputCopy = { ...functionInput, routingslip };
-    // result.push(context);
-    // result.push(contextCopy);
-    result.push(functionInput);
-    result.push(functionInput);
-    // result.push(functionInputCopy);
+    cloudEvent["routingslip"] = [['wire-tap']];
+    result.push(JSON.parse(functionInput));
+    result.push(cloudEvent);
 
-    // producer.on('ready', function () {
-    //     producer.send([{ topic: 'wire-tap-channel', messages: [contextCopy] }], function (err, data) { });
-    // });
-
-    // return { status: "done" }
     return result;
 }
