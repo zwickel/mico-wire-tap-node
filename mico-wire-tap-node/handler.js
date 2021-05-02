394ba69c61bee53bae63fbacb0f8c5ec3b12221f
ma <mariusaltmann@outlook.de>
2021-05-03 00:06:41 +0200

    formatting

--- mico-wire-tap-node/handler.js
+++ mico-wire-tap-node/handler.js
@@ -4,7 +4,9 @@ module.exports = async (functionInput, callback) => {
     const cloudEvent = JSON.parse(functionInput)
 
     let result = [];
+
     cloudEvent["routingslip"] = [['wire-tap']];
+
     result.push(JSON.parse(functionInput));
     result.push(cloudEvent);
 
